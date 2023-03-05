7f60ac5da7925fc49db2260e184a7121eaaa4dc9
zhouzl <zzl19911220@163.com>
2023-03-05 03:49:50 +0000

    去除无效提交

--- src/hello.h
+++ src/hello.h
@@ -1,3 +1,22 @@
+// C++:编写跨平台程序的关键，C/C++中的内置宏定义
+// 分两部分：
+ 
+// 操作系统判定：
+ 
+// Windows:   WIN32
+ 
+// Linux:   linux
+ 
+// Solaris:   __sun
+ 
+// 编译器判定：
+ 
+// VC:  _MSC_VER
+ 
+// GCC/G++:   __GNUC__
+ 
+// SunCC:   __SUNPRO_C和__SUNPRO_CC
+
 #ifndef __HELLO_H__
 #define __HELLO_H__
 #ifdef  _MSC_VER
@@ -6,7 +25,7 @@
 #else
 #define LIBHELLO_API __declspec(dllimport)
 #endif // #ifdef LIBHELLO_BUILD
-#else
+#else //__GNUC__
 #define LIBHELLO_API
 #endif // #ifdef WIN32
 LIBHELLO_API void Hello2(const char* text);
